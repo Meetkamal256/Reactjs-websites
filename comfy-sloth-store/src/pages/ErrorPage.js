63d55bed80a8a34788db861abba638737a5a8a36
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-19 14:47:54 +0100

    Error page layout and styling done

--- comfy-sloth-store/src/pages/ErrorPage.js
+++ comfy-sloth-store/src/pages/ErrorPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 const ErrorPage = () => {
-
+  
   return <Wrapper className='page-100'>
     <section>
       <h1>404</h1>
