a120624397a3e6b6cd26734bd90b579cf61a382d
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-20 10:32:55 +0100

    contact component layout and styling done

--- comfy-sloth-store/src/components/Contact.js
+++ comfy-sloth-store/src/components/Contact.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <>contact section</>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h2>Join our newsletter and get 20% off</h2>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint
+            voluptas ipsum id iusto voluptatum deleniti cumque odio officia
+            molestias.
+          </p>
+          <form className="contact-form">
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button className="submit-btn">Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +88,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
